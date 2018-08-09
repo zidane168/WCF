fa98058eba3bb686f02671315f5a55e3ab7a17e9
zidane168 <huuvi168@gmail.com>
2018-08-09 21:50:42 +0700

    - Update HelloServices

---
+++ HelloService/HelloServiceHost/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+
+// netsh http add urlacl url=http://+:80/ user=DOMAIN\user
+// wcf-servicehost-access-right
+// netsh http add urlacl url=http://+:8080/ user=vilh
+
+// netsh http add urlacl url=http://+:8080/ user=vilh
+// netsh http add urlacl url=http://localhost:8080 user=vilh
+
+// I closed Visual studio IDE and reopened it by right clicking on the Visual Studio icon and saying "Run as Administrator", Then when I ran the host, It worked!!!
+// https://stackoverflow.com/questions/8727293/http-could-not-register-url-http-8000-hellowcf-your-process-does-not-have
+
+namespace HelloServiceHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ServiceHost host = new ServiceHost(typeof(HelloService.HelloService)))
+            {
+                host.Open();
+
+                Console.WriteLine("Host Created @ " + DateTime.Now.ToString());
+                Console.ReadLine();
+
+
+            }
+        }
+    }
+}
